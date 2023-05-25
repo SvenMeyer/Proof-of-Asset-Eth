1c89e04f53d544c681ebf39584c1aead69c67a40
Sven Meyer <git_dev@svenmeyer.com>
2023-05-25 23:12:39 +1000

    fix: replace seed with environment variable

--- gcli.sh
+++ gcli.sh
@@ -1,4 +1,4 @@
 # start ganache-cli
 
-ganache-cli -m "tomorrow draft giggle design purchase daring goddess cute inquiry giant thumb journey" --networkId 5777 --port 7545
+ganache --wallet.mnemonic "$MNEMONIC_DEV" --networkId 5777 --port 7545
 
